19c6e26932ae7d456286644a37240b377a5a79b8
Una Kravets <una.kravets@gmail.com>
2017-03-18 16:49:09 -0500

    comparison step, checking against CSS file

--- build/main.js
+++ build/main.js
@@ -87,7 +87,7 @@ var compiledStyles = fs.readFileSync(cssFilePath, 'utf8');
 
 // For the computed CSS file, grab the unique elements
 var cssElemArray = getCSSElems(compiledStyles);
-var htmlElemArrays = [];
+var eachHTMLElemArray = [];
 
 // For each HTML file, run through and grab the elements
 glob(htmlFilePath, function (er, files) {
@@ -95,15 +95,24 @@ glob(htmlFilePath, function (er, files) {
 
   files.forEach(function (file) {
     var htmlText = fs.readFileSync(file, 'utf8');
-    htmlElemArrays.push(getHTMLElems(htmlText));
+    eachHTMLElemArray.push(getHTMLElems(htmlText));
   });
 
   // Merge all of the elements from each HTML file into one array
-  var allHTMLElems = [].concat(_toConsumableArray(new Set((_ref = []).concat.apply(_ref, htmlElemArrays))));
-  console.log(allHTMLElems);
-});
+  var totalHTMLElemArray = [].concat(_toConsumableArray(new Set((_ref = []).concat.apply(_ref, eachHTMLElemArray))));
 
-// console.log(cssElemArray, htmlElemArray);
+  // This should be in its own function probably, but keeping
+  // it here because re: scope lol, this is easier for now
+  console.log('All CSS Allowed: \n' + cssElemArray, '\n\nAll HTML Classes/Ids Used: \n' + totalHTMLElemArray);
+  //TO DO: Add elements from accepted elements list to the total CSS array
+
+  // Comparing total HTML to CSS Elements allowed in design system
+  var unmatchedElems = totalHTMLElemArray.filter(function (obj) {
+    return cssElemArray.indexOf(obj) == -1;
+  });
+
+  console.log('    Your markup includes the following elements\n    that do not live in the design system or\n    accepted classes file:\n\n    ' + unmatchedElems);
+});
 
 /***/ },
 /* 1 */
--- src/index.js
+++ src/index.js
@@ -11,18 +11,30 @@ const compiledStyles = fs.readFileSync(cssFilePath, 'utf8');
 
 // For the computed CSS file, grab the unique elements
 const cssElemArray = getCSSElems(compiledStyles);
-let htmlElemArrays = [];
+let eachHTMLElemArray = [];
 
 // For each HTML file, run through and grab the elements
 glob(htmlFilePath, (er, files) => {
   files.forEach(file => {
     const htmlText = fs.readFileSync(file, 'utf8');
-    htmlElemArrays.push(getHTMLElems(htmlText));
+    eachHTMLElemArray.push(getHTMLElems(htmlText));
   });
 
   // Merge all of the elements from each HTML file into one array
-  const allHTMLElems = ([ ...new Set( [].concat( ...htmlElemArrays ) ) ]);
-  console.log(allHTMLElems);
-});
+  const totalHTMLElemArray = ([ ...new Set( [].concat( ...eachHTMLElemArray ) ) ]);
 
-// console.log(cssElemArray, htmlElemArray);
+  // This should be in its own function probably, but keeping
+  // it here because re: scope lol, this is easier for now
+  console.log('All CSS Allowed: \n' + cssElemArray, '\n\nAll HTML Classes/Ids Used: \n' + totalHTMLElemArray);
+  //TO DO: Add elements from accepted elements list to the total CSS array
+
+  // Comparing total HTML to CSS Elements allowed in design system
+  const unmatchedElems = totalHTMLElemArray.filter(function(obj) { return cssElemArray.indexOf(obj) == -1; });
+
+  console.log(
+    `    Your markup includes the following elements
+    that do not live in the design system or
+    accepted classes file:
+
+    ${unmatchedElems}`);
+});
